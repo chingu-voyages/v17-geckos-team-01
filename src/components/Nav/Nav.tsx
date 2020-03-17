4e18782cc61a23c858237332c5700d4425fbc382
Sean Norwood <norwood.sean@gmail.com>
2020-03-17 16:41:46 -0500

    refactor(next): setup nextjs bootstrap

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Nav.module.scss"
+
+const Nav = () => (
+  <div className="container">
+    <ul>
+      <li>Button 1</li>
+      <li>Button 2</li>
+    </ul>
+  </div>
+);
+
+export default Nav;
---
+++ src/pages/Index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Index.module.scss';
+import { Nav, Hero } from '../component.exports';
+
+const Index = () => (
+  <>
+    <Nav />
+    <Hero />
+  </>
+);
+
+export default Index;
