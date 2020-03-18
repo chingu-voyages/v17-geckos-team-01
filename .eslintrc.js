ba8853d1b182caf82210bde24d5fb87a917d44e8
Sean Norwood <norwood.sean@gmail.com>
2020-03-18 15:20:37 -0500

    refactor: sets up linting to use eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,24 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-    'react'
-  ],
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
-    'airbnb-typescript',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
-    project: './tsconfig.json'
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
   },
   rules: {
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "react/prop-types": "off",
-    "import/extensions": "off",
-  }
+    'react/prop-types': 'off',
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
 };
