3a389b78b41cdf7252e504c0e3d8334609f937ab
Sean Norwood <norwood.sean@gmail.com>
2020-03-18 15:29:04 -0500

    refactor: adds linting and prettier precommit hook

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -10,7 +10,7 @@ const Nav: React.FC = () => (
       <Menu mode="horizontal" defaultSelectedKeys={['1']}>
         <Menu.Item key="1">nav 1</Menu.Item>
         <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3 </Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
         <Menu.Item key="4">nav 4</Menu.Item>
         <Menu.Item key="5">nav 5</Menu.Item>
       </Menu>
