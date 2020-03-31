257ef753c80426cf836b1e128088113a6fbe6318
Sean Norwood <norwood.sean@gmail.com>
2020-03-31 12:17:11 -0500

    refactor: revert mobx stuff

--- src/pages/Listings.tsx
+++ src/pages/Listings.tsx
@@ -6,19 +6,14 @@ import {
   CardContent,
   CardHeader,
   Grid,
-  ListItem,
-  ListItemText,
   makeStyles,
   Typography,
 } from '@material-ui/core';
 import fetch from 'isomorphic-unfetch';
 import { NextPage } from 'next';
 import Link from 'next/link';
-import React, { Component } from 'react';
-import { Property } from 'typings/propertyAddressTypes';
+import React from 'react';
 import { Nav } from '../component.exports';
-import { inject, observer } from 'mobx-react';
-import { render } from 'react-dom';
 
 const useStyles = makeStyles({
   root: {
@@ -31,67 +26,6 @@ const useStyles = makeStyles({
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
   },
 });
-
-// @inject('addressStore')
-// @observer
-// class Listings extends Component {
-//   // const classes = useStyles();
-//   static async getInitialProps({ mobxStore, query }) {
-//     await mobxStore.addressStore.fetchData(query.id);
-//     return { address: mobxStore.addressStore.address };
-//   }
-
-//   render(): React.ReactElement {
-//     const { address } = this.props;
-//     return (
-//       <>
-//         <Nav navBarTitle="Listings" />
-//         <Grid
-//           style={{ marginTop: '3px' }}
-//           alignItems="flex-start"
-//           justify="center"
-//           direction="row"
-//           container
-//           spacing={2}
-//         >
-//           {address.map(({ identifier: id, address: address }) => (
-//             <>
-//               <Grid style={{ maxWidth: 345 }} key={id.obPropId} item sm={3} xs={12}>
-//                 <Card className={classes.paper} elevation={1}>
-//                   <CardActionArea>
-//                     <Link href="/p/[id]" as={`/p/${id.obPropId}`}>
-//                       <CardHeader subheader={address.line2} title={address.line1}>
-//                         <a>{address.line1}</a>
-//                       </CardHeader>
-//                     </Link>
-//                   </CardActionArea>
-//                   <CardContent>
-//                     {/* <ListItem>
-//                       <ListItemText primary={summary.absenteeInd} />
-//                     </ListItem>
-//                     <ListItem>
-//                       <ListItemText primary={summary.propclass} />
-//                     </ListItem>
-//                     <ListItem>
-//                       <ListItemText primary={summary.propsubtype} />
-//                     </ListItem>
-//                     <ListItem>
-//                       <ListItemText primary={summary.yearbuilt} />
-//                     </ListItem> */}
-//                   </CardContent>
-//                   <CardActions>
-//                     <Button size="small">Share</Button>
-//                     <Button size="small">Learn More</Button>
-//                   </CardActions>
-//                 </Card>
-//               </Grid>
-//             </>
-//           ))}
-//         </Grid>
-//       </>
-//     );
-//   }
-// }
 
 const Listings: NextPage = ({ data }: any) => {
   const classes = useStyles();
@@ -134,19 +68,17 @@ const Listings: NextPage = ({ data }: any) => {
 };
 
 Listings.getInitialProps = async function () {
-  const address = await fetch(
-    'http://api.gateway.attomdata.com/propertyapi/v1.0.0/property/address?postalcode=82009&page=1&pagesize=10',
-    { headers: { accept: 'application/json', apikey: '236e138b3c80fc33a9245df42338e83e' } },
-  );
-  const summary = await fetch(
-    'https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/detailwithschools?id=16237759912033',
-    { headers: { accept: 'application/json', apikey: '236e138b3c80fc33a9245df42338e83e' } },
-  );
+  const address = await fetch('http://127.0.0.1:3100/address', {
+    headers: { accept: 'application/json', apikey: '236e138b3c80fc33a9245df42338e83e' },
+  });
+  const summary = await fetch('http://127.0.0.1:3100/detailwithschools?id=16237759912033', {
+    headers: { accept: 'application/json', apikey: '236e138b3c80fc33a9245df42338e83e' },
+  });
   const addressData = await address.json();
   const summaryData = await summary.json();
 
-  const combinedData = [...addressData.property, ...summaryData.property];
-  // console.log(combinedData);
+  const combinedData = [...addressData, ...summaryData.property];
+  console.log(addressData);
 
   return {
     // properties: data.property.map((entry) => entry),
